4c3cf02ffa807f111c449147f5afe8584bc5b5cd
Nathan Walters <nwalters512@gmail.com>
2019-05-01 12:45:46 -0500

    Dump more request info to logs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3052;
 
+app.set('trust proxy', true)
+
 app.get("/", (req, res) => {
   for (let i = 0; i < 200; i++) {
     res.cookie(`c${i}`, `${i}`);
@@ -11,6 +13,8 @@ app.get("/", (req, res) => {
 
 app.get('/headers', (req, res) => {
   console.log(req.headers);
+  console.log(req.protocol);
+  console.log(req.secure);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
